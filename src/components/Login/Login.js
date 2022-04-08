5c13258efe22b17798fac4e2eec218c30f0ac748
bishwajit70 <mailtobishwa15@gmail.com>
2022-04-09 02:12:09 +0600

    something interesting added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,23 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
-import useFirebase from '../hooks/useFirebase';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import { useLocation, useNavigate } from 'react-router-dom';
+import app from '../../firebase.init';
 import './Login.css'
+const auth = getAuth(app)
 
 const Login = () => {
-    const {signInWithGoogle} = useFirebase()
+    const [signInWithGoogle, user] = useSignInWithGoogle(auth)
+    const location = useLocation()
+    const navigate = useNavigate()
+    const from = location?.state?.from?.pathname || '/'
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+            .then(() => {
+                navigate(from, { replace: true })
+            })
+    }
+
     return (
         <div>
             <h2 className='text-2xl pb-4'>Please Login</h2>
@@ -12,7 +26,7 @@ const Login = () => {
                 <input className='border p-3 w-full md:w-1/4 grid mx-auto mb-5' type="password" name="password" id="password" placeholder='password' />
                 <input className='border p-3 w-full cursor-pointer bg-orange-500 hover:bg-orange-400 text-white rounded-md text-xl md:w-1/4 grid mx-auto' type="submit" value="Login" />
             </form>
-            <button onClick={signInWithGoogle} className='border  mt-5 p-3 w-full cursor-pointer bg-blue-600 hover:bg-blue-500 text-white rounded-md text-xl md:w-1/4 grid mx-auto' > Sign In with Google </button>
+            <button onClick={handleGoogleSignIn} className='border  mt-5 p-3 w-full cursor-pointer bg-blue-600 hover:bg-blue-500 text-white rounded-md text-xl md:w-1/4 grid mx-auto' > Sign In with Google </button>
 
         </div >
     );
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,18 +2,17 @@ import { getAuth } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import app from '../../firebase.init';
-import './Home.css'
 
 const auth = getAuth(app)
-
-const Home = () => {
+const Products = () => {
 
     const [user] = useAuthState(auth)
     return (
         <div>
-            <h2>Current User is: {user ? user.displayName : 'No body'}</h2>
+            <h2>This is product Page</h2>
+            <h3>{user ? user.displayName : 'No body'}</h3>
         </div>
     );
 };
 
-export default Home;
+export default Products;
